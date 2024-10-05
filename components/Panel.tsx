18dfe7855f539c00ff499aef7e457a08579b16ee
lamdanghoang <danghlambk14@gmail.com>
2024-10-05 11:31:19 +0700

    edit panel

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -10,7 +10,7 @@ const Panel: React.FC<Props> = ({ children, title, height }) => {
     const classes = `${height} w-full`;
     return (
         <section className="pb-1 rounded-lg shadow-md relative bg-gray-800/40">
-            <h3 className="z-0 sticky top-0 p-2 rounded-t-lg cursor-move z-10 transition-all duration-300 ease-in-out flex items-center text-sm font-semibold bg-lido/90 text-gray-200 backdrop-blur-sm">
+            <h3 className="z-0 sticky top-0 p-2 rounded-t-lg cursor-move transition-all duration-300 ease-in-out flex items-center text-sm font-semibold bg-lido/90 text-gray-200 backdrop-blur-sm">
                 {title}
             </h3>
             <ScrollArea className={classes}>
